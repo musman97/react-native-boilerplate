0005cc075b9e51fdca8a93a7955bf32c9d2e7458
Muhammad Usman <zahoor.usman.m@gmail.com>
2024-07-27 18:08:06 +0500

    Revert "chore: Bump react query version"
    This reverts commit 39713688f4ad0a4325d3df6211c9138396e7e62a.

--- template/App.tsx
+++ template/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {QueryClientProvider} from '@tanstack/react-query';
+import {QueryClientProvider} from 'react-query';
 import {queryClient} from '~/core';
 import RootNavigator from '~/navigation';
 
--- template/src/core/react-query/types.ts
+++
@@ -1,8 +0,0 @@
-import '@tanstack/react-query';
-import {ApiFailureResult} from '../api';
-
-declare module '@tanstack/react-query' {
-  interface Register {
-    defaultError: ApiFailureResult;
-  }
-}
